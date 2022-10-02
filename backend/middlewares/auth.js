524a678df27416ace06ea1f9fdf9448ba6538b4e
LubchikEgor <97447426+SpichkaXD@users.noreply.github.com>
2022-10-02 13:39:32 +0300

    fix auth5

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -9,7 +9,7 @@ module.exports.auth = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, `${NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'}`);
   } catch (err) {
     next(new UnauthorizedError('Токен не найден'));
   }
