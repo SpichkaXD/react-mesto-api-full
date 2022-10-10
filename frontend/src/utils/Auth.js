1f4f8d9fb09bec4d6ffd654ca8c971488e4bdc88
LubchikEgor <97447426+SpichkaXD@users.noreply.github.com>
2022-10-10 18:44:49 +0300

    front

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,51 +1,47 @@
-export const BASE_URL = 'https://api.spichka.nomoredomains.icu';
+export const BASE_URL = "https://api.spichka.nomoredomains.icu";
 
 export const handleResponse = (res) => {
-  if (res.ok) {
-    return res.json();
-}
-  return Promise.reject(res.status);
+    if (res.ok) {
+        return res.json();
+    }
+    return Promise.reject(res.status);
 };
 
-export const headers =  {
-  'Accept': 'application/json',
-  'Content-Type': 'application/json'
+export const headers = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
 };
 
-export const register = ({email, password}) => {
-  return fetch(`${BASE_URL}/signup`, {
-    method: 'POST',
-    headers,
-    credentials: "include",
-    body: JSON.stringify({email, password})
-  })
-  .then(res => handleResponse(res));
-}
+export const register = ({ email, password }) => {
+    return fetch(`${BASE_URL}/signup`, {
+        method: "POST",
+        headers,
+        credentials: "include",
+        body: JSON.stringify({ email, password }),
+    }).then((res) => handleResponse(res));
+};
 
-export const authorize = ({email, password}) => {
-  return fetch(`${BASE_URL}/signin`, {
-    method: 'POST',
-    headers,
-    credentials: "include",
-    body: JSON.stringify({email, password})
-  })
-  .then(res => handleResponse(res));
-}
+export const authorize = ({ email, password }) => {
+    return fetch(`${BASE_URL}/signin`, {
+        method: "POST",
+        headers,
+        credentials: "include",
+        body: JSON.stringify({ email, password }),
+    }).then((res) => handleResponse(res));
+};
 
 export const getEmail = () => {
     return fetch(`${BASE_URL}/users/me`, {
-      method: 'GET',
-      headers, 
-      credentials: "include"
-    })
-    .then(res => handleResponse(res));
-  }
+        method: "GET",
+        headers,
+        credentials: "include",
+    }).then((res) => handleResponse(res));
+};
 
-  export const logOut = () => {
+export const logOut = () => {
     return fetch(`${BASE_URL}/signout`, {
-      method: 'POST',
-      headers,
-      credentials: "include",
-    })
-    .then(res => handleResponse(res));
-  }
+        method: "POST",
+        headers,
+        credentials: "include",
+    }).then((res) => handleResponse(res));
+};
